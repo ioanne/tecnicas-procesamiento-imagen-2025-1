6ae777ee6dfc4f7b1f34a6b440289b1518349332
Juan Ignacio Bonini <juanignaciobonini@hotmail.com>
2025-05-21 20:10:29 -0300

    Subir un archivo

--- clase_api/app/main.py
+++ clase_api/app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File, Form
 
 from app.routers import usuarios, alumnos
 
@@ -12,3 +12,15 @@ app.include_router(alumnos.router, prefix="/alumnos", tags=["alumnos"])
 @app.get("/")
 async def hola_mundo():
     return {"mensaje":"Hola mundo!"}
+
+
+@app.post("/subir_imagen")
+async def subir_imagen(nombre: str = Form(...), imagen: UploadFile = File(...)):
+    contenido = await imagen.read()
+    
+    with open(f"{nombre}_{imagen.filename}", "wb") as f:
+        # Lo que quiera con el archivo abierto
+        f.write(contenido)
+    # Se cierra el archivo.
+    
+    return {"mensaje": "Imagen subida exitosamente"}
